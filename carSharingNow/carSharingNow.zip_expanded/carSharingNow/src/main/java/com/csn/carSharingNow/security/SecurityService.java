6b61cbf438a53470e5fc3bbb486ae2e1916df495
SebvM <sebastianvonminden@gmail.com>
2022-07-24 13:20:07 +0200

    ablage sicher für abnahme ohne tests

--- carSharingNow/carSharingNow.zip_expanded/carSharingNow/src/main/java/com/csn/carSharingNow/security/SecurityService.java
+++ carSharingNow/carSharingNow.zip_expanded/carSharingNow/src/main/java/com/csn/carSharingNow/security/SecurityService.java
@@ -1,5 +1,8 @@
 package com.csn.carSharingNow.security;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +12,7 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Component;
-
+import com.csn.carSharingNow.models.Role;
 import com.csn.carSharingNow.models.User;
 import com.csn.carSharingNow.repositories.UserRepository;
 import com.vaadin.flow.component.UI;
@@ -28,8 +31,9 @@ import com.vaadin.flow.server.VaadinServletRequest;
 public class SecurityService {
     
     @Autowired
-    private UserRepository userRepository;
-
+    private UserRepository userRepository;    
+  
+    
     public static Optional<Authentication> getAuthentication() {
         SecurityContext context = SecurityContextHolder.getContext();
         return Optional.ofNullable(context.getAuthentication())
@@ -45,5 +49,14 @@ public class SecurityService {
         SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
         logoutHandler.logout(VaadinServletRequest.getCurrent().getHttpServletRequest(), null, null);
     }
-   
+    
+    public void getFirstTimeUser() {
+    	List<Role> adminroles = new ArrayList<Role>();
+        adminroles.add(Role.ADMIN);
+        Date dateofDeath = new Date(System.currentTimeMillis());
+    	User defaultUser = new User("admin", "admin@cSn.de", "rotierendeente", "Admin", "Adminson", dateofDeath, adminroles);
+    	userRepository.save(defaultUser);
+        System.out.println(defaultUser);
+		
+    }
 }
